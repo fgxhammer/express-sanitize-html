71d76a8fef735b4105b5dc9c41b870d3e896c54b
Franz Gollhammer <franz.gollhammer@gmail.com>
2021-10-14 15:05:18 +0200

    More generic approach

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
 // Request body sanitize dirty content
 // Always put after body parser [express.json()]
 const sanitizeHtml = require('sanitize-html')
-const config = {}
 
-const sanitize = (val) => {
-  if (typeof val !== 'string') {
-    return val
+module.exports = function (config) {
+  
+  const sanitize = (val) => {
+    if (typeof val !== 'string') {
+      return val
+    }
+  
+    try {
+      const dirty = val
+      const clean = sanitizeHtml(val, config)
+  
+      return clean
+    } catch (e) {
+      console.error('Sanitization failed', val, e)
+      const error = new Error({
+        message: 'Failed to sanitize html',
+      })
+      return val
+    }
   }
-
-  try {
-    const dirty = val
-    const clean = sanitizeHtml(val, config)
-
-    return clean
-  } catch (e) {
-    console.error('Sanitization failed', val, e)
-    const error = new Error({
-      message: 'Failed to sanitize html',
-    })
-    return val
-  }
-}
-
-// Iterate over body object
-const iterateRecursiveAndSanitize = obj => {
-  for (const i in obj) {
-    if (typeof obj[i] === 'object' && obj[i] !== null) {
-      iterateRecursiveAndSanitize(obj[i])
-    } else {
-      if (obj[i]) {
-        obj[i] = sanitize(obj[i])
+  
+  // Iterate over body object
+  const iterateRecursiveAndApplyFun = (obj, fun) => {
+    for (const i in obj) {
+      if (typeof obj[i] === 'object' && obj[i] !== null) {
+        iterateRecursiveAndApplyFun(obj[i])
+      } else {
+        if (obj[i]) {
+          obj[i] = fun(obj[i])
+        }
       }
     }
   }
-}
-
-// Middleware function
-module.exports = (req, res, next) => {
-  if (req.body) {
-    iterateRecursiveAndSanitize(req.body)
+  
+  return function (req, res, next) {
+    if (req.body) {
+      iterateRecursiveAndApplyFun(req.body, sanitize)
+    }
+  
+    next()
   }
-
-  next()
+  
 }
+
+
